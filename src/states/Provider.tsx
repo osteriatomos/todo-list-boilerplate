7f524d0ed2bee1995258aae486ed672f14f63d19
OsteriaTomos <osteriatomos@gmail.com>
2020-11-06 00:03:30 +0900

    create states context provider

---
+++ src/states/Provider.tsx
@@ -0,0 +1,23 @@
+import React, { createContext } from "react";
+import { useTodo } from "./todos/hooks";
+import { DispatchActions, Todo } from "./todos/types";
+
+export const TodoContext = createContext(
+  {} as {
+    state: Todo[];
+    dispatchActions: DispatchActions;
+    isFetching: boolean;
+    errorMessage: string;
+  }
+);
+
+export const ContextProvider: React.FC = ({ children }) => {
+  const [state, dispatchActions, isFetching, errorMessage] = useTodo();
+  return (
+    <TodoContext.Provider
+      value={{ state, dispatchActions, isFetching, errorMessage }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
