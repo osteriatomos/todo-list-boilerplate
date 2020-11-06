7a3efb5f152ed91536e3e6a90c2667ab2bb0ef4c
OsteriaTomos <osteriatomos@gmail.com>
2020-11-07 00:58:22 +0900

    delete unused code

--- src/states/todos/hooks.ts
+++ src/states/todos/hooks.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useReducer } from "react";
+import { useState, useReducer } from "react";
 import { initialState, reducer } from "./reducers";
 import { Todo, DispatchActions } from "./types";
 import { createTodoAction, deleteTodoAction } from "./actions";
@@ -8,35 +8,28 @@ export const useTodo = (): [Todo[], DispatchActions, boolean, string] => {
   const [errorMessage, setErrorMessage] = useState("");
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const createTodo = useCallback(
-    async ({ actionItem }: { actionItem: string }) => {
-      setIsFetching(true);
-      try {
-        // await 非同期処理
-        setIsFetching(false);
-        dispatch(createTodoAction({ actionItem: actionItem }));
-      } catch (e) {
-        setIsFetching(false);
-        setErrorMessage(e);
-      }
-    },
-    [isFetching, errorMessage]
-  );
-
-  const deleteTodo = useCallback(
-    async ({ id }: { id: number }) => {
-      setIsFetching(true);
-      try {
-        // await 非同期処理
-        setIsFetching(false);
-        dispatch(deleteTodoAction({ id: id }));
-      } catch (e) {
-        setIsFetching(false);
-        setErrorMessage(e);
-      }
-    },
-    [isFetching, errorMessage]
-  );
+  const createTodo = async ({ actionItem }: { actionItem: string }) => {
+    setIsFetching(true);
+    try {
+      // await 非同期処理
+      setIsFetching(false);
+      dispatch(createTodoAction({ actionItem: actionItem }));
+    } catch (e) {
+      setIsFetching(false);
+      setErrorMessage(e);
+    }
+  };
+  const deleteTodo = async ({ id }: { id: number }) => {
+    setIsFetching(true);
+    try {
+      // await 非同期処理
+      setIsFetching(false);
+      dispatch(deleteTodoAction({ id: id }));
+    } catch (e) {
+      setIsFetching(false);
+      setErrorMessage(e);
+    }
+  };
 
   const dispatchActions = {
     create: createTodo,
