44f90487de34e4cfa313ce33bcd3ae606b514107
OsteriaTomos <osteriatomos@gmail.com>
2020-11-08 16:21:06 +0900

    set useCallback to hooks

--- src/states/todos/hooks.ts
+++ src/states/todos/hooks.ts
@@ -1,4 +1,4 @@
-import { useState, useReducer } from 'react';
+import { useState, useReducer, useCallback } from 'react';
 import { initialState, reducer } from './reducers';
 import { CREATE_TODO, DELETE_TODO, Todo, DispatchActions } from './types';
 
@@ -7,19 +7,22 @@ export const useTodo = (): [Todo[], DispatchActions, boolean, string] => {
   const [errorMessage, setErrorMessage] = useState('');
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const createTodo = async ({ actionItem }: { actionItem: string }) => {
-    setIsFetching(true);
-    try {
-      // await 非同期処理
-      setIsFetching(false);
-      dispatch({ type: CREATE_TODO, payload: { actionItem: actionItem } });
-    } catch (e) {
-      setIsFetching(false);
-      setErrorMessage(e);
-    }
-  };
+  const createTodo = useCallback(
+    async ({ actionItem }: { actionItem: string }) => {
+      setIsFetching(true);
+      try {
+        // await 非同期処理
+        setIsFetching(false);
+        dispatch({ type: CREATE_TODO, payload: { actionItem: actionItem } });
+      } catch (e) {
+        setIsFetching(false);
+        setErrorMessage(e);
+      }
+    },
+    []
+  );
 
-  const deleteTodo = async ({ id }: { id: number }) => {
+  const deleteTodo = useCallback(async ({ id }: { id: number }) => {
     setIsFetching(true);
     try {
       // await 非同期処理
@@ -29,7 +32,7 @@ export const useTodo = (): [Todo[], DispatchActions, boolean, string] => {
       setIsFetching(false);
       setErrorMessage(e);
     }
-  };
+  }, []);
 
   const dispatchActions = {
     create: createTodo,
